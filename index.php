a66f39d6b65b6240368506bd041d2808b1927be1
Paolo <paolo.meola@gmail.com>
2013-09-05 23:54:53 -0700

    smile

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
-include('youtube.class.php');
-
-$youtube = new YouTube();
-
-$searchResults = $youtube->getSearchResults('apple');
-
-var_dump($searchResults);
+    error_reporting(E_ALL);
+    ini_set('error_reporting', E_ALL);
+    include('youtube.class.php');
+    $youtube = new YouTube();
+    $searchResults = $youtube->getSearchResults('apple');
+    var_dump($searchResults);    
+    echo "your sister!";
 ?>
